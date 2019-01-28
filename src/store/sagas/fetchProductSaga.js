9e7015e65d70792ede22cf00ade92a464c6c5072
ksukoz <seniurbano@gmail.com>
2019-01-28 21:08:18 +0200

    Fix id in product item reviews

--- src/store/sagas/fetchProductSaga.js
+++ src/store/sagas/fetchProductSaga.js
@@ -1,20 +1,16 @@
-import { takeEvery, put, call } from "redux-saga/effects";
-import { getProduct } from "../actions/catalogActions";
-import { GET_PRODUCT_ID } from "../actions/types";
+import { takeLatest, put, call } from 'redux-saga/effects';
+import { getProduct } from '../actions/catalogActions';
+import { GET_PRODUCT_ID } from '../actions/types';
 
 export function* fetchProductSaga(item) {
-  const { payload } = item;
-  if (payload) {
-    const response = yield call(
-      fetch,
-      `http://kawaapi.gumione.pro/api/catalog/item/${payload}`
-    );
-    const { item } = yield response.json();
-    console.log(item);
-    yield put(getProduct(item));
-  }
+	const { payload } = item;
+	if (payload) {
+		const response = yield call(fetch, `http://kawaapi.gumione.pro/api/catalog/item/${payload}`);
+		const { item } = yield response.json();
+		yield put(getProduct(item));
+	}
 }
 
 export function* watchFetchProductSaga() {
-  yield takeEvery(GET_PRODUCT_ID, fetchProductSaga);
+	yield takeLatest(GET_PRODUCT_ID, fetchProductSaga);
 }
